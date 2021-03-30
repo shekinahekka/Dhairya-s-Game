6a6cb6c528e547f05f048398aeb5159cb8727faf
shekinahekka <71063335+shekinahekka@users.noreply.github.com>
2021-03-30 15:56:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,20 +1,7 @@
-
-
 //BUGS
-//function birds: birds not stopping after end state
-//player is jumping even after end state
-//slide thing not working 
-
-//TO BE DONE
-//two jumps thing 
-
-//End state in draw();
-//Play state in draw();
-//score using coins
-//highest coins collected(highest score)
+//the text for highest score thing is not getting displayed
+//wow reaction 
 //moving of background to be done 
-//GameOver button
-
 
 
 
@@ -27,11 +14,18 @@ var ground;
 var obstacleGroup, obstacle1, obstacle1Img;
 var score = 0;
 
-//
 var GameOver, GameOverImg;
 var birdGrp, bird1, bird1Img;
 
-var slide;
+//var slide;
+
+var coin, coinImg, coinGroup;
+
+var wow, wowImg;
+var x;
+
+localStorage['HighestScore'] = 0;
+var jumpSound, collectSound, overSound;
 function preload(){
 
 runImg = loadAnimation("run/0.png","run/1.png","run/2.png","run/3.png","run/4.png");
@@ -43,14 +37,20 @@ bgImg = loadImage("anime bg.png");
 obstacle1Img=loadImage("barricade.png");
 bird1Img=loadAnimation("Bird/0.png","Bird/1.png","Bird/2.png","Bird/3.png","Bird/4.png","Bird/7.png","Bird/8.png","Bird/9.png","Bird/10.png","Bird/11.png","Bird/16.png","Bird/17.png","Bird/18.png","Bird/19.png","Bird/20.png","Bird/29.png","Bird/30.png","Bird/31.png","Bird/32.png","Bird/33.png","Bird/38.png","Bird/39.png")
 GameOverImg = loadImage("gameover.png");
+coinImg=loadImage("coin.png");
+wowImg=loadImage("wow.png");
+jumpSound = loadSound("jump.wav");
+collectSound=loadSound("coin collect.mp3");
+overSound=loadSound("gO.wav");
 }
+
 function setup() {
   createCanvas(displayWidth,displayHeight-150);
- bg = createSprite(500,200,displayWidth,displayHeight);
- bgImg.resize(displayWidth, displayHeight)
- bg.addImage(bgImg);
- //bg.x = bg.width / 2;
- bg.scale = 1.5
+  
+  bg = createSprite(500,200,displayWidth,displayHeight);
+  bgImg.resize(displayWidth, displayHeight)
+  bg.addImage(bgImg);
+  bg.scale = 1.5;
 
   ground=createSprite(288,550,1000,20);
   ground.velocityX = -(6 + 3*score/100);
@@ -65,68 +65,92 @@ function setup() {
 
   obstacleGroup = new Group();
   birdGrp=new Group();
+  coinGroup=new Group();
 
   GameOver = createSprite(displayWidth-680,displayHeight-430);
   GameOver.scale=1.1;
   GameOver.addImage(GameOverImg);
+
   score = 0;
+
+  wow=createSprite(200,200);
+  //wow.scale=0.5;
+  wow.addImage("wow", wowImg);
 
 
 }
 
-function draw() {
-  //player.debug = true;
+function draw(){
   
   background("white");
   //background(bgImg);
   //to be removed after positioning 
   text(mouseX + ',' + mouseY, 10, 15);
- 
+
  if(gameState===PLAY){
   GameOver.visible=false;
-  score = frameCount % 200
+  wow.visible=false;
   if (ground.x < 0) {
     ground.x = ground.width / 2;
   }
   ground.velocityX = -4;
 
- if(bg.x < 330){
+
+  if(bg.x < 330){
     bg.x = bg.width/2;
   }
  bg.velocityX = -4;
- console.log(bg.x);
- // ground.visible = false;
 
- 
+
+
+ if (score > parseInt(localStorage.getItem('highestScore'), 10)) {
+  localStorage.setItem('highestScore', score);
+}
  if(keyDown("up")&& player.y >=250) {
   player.velocityY = -15;
+  jumpSound.play();
 }
 player.velocityY = player.velocityY + 0.8;
-/*if(keyDown("down")){
-  player.changeAnimation("slide",slideImg);
-}
-console.log(player.velocityY);
-*/
 
 if(obstacleGroup.isTouching(player)||birdGrp.isTouching(player)){
- gameState=END;
- sliding();
+  overSound.play();
+  gameState=END;
+
 }
  player.collide(ground);
  enemy();
  birds();
- 
+ points();
 
-}
+ if(coinGroup.isTouching(player)){
+ score= score+10;
+ /*textSize(30);
+ strokeWeight(4);
+ fill("white");
+ text("Great!", 500,20);
+ */
+ wow.depth = bg.depth;
+ wow.depth+=10;
+ wow.x=200;
+ wow.y=200;
+ coinGroup.destroyEach();
+ collectSound.play();
+// wow.visible=true;
+ //x = setInterval(wow, 6000);
+ }
+ textSize(35);
+ textFont("Monospace");
+ textStyle(BOLD);
+ fill("#0090ff");
+ text("Score = " + score + " Points" , 200,50);
+
+ } 
  
  else if(gameState === END){
-
-  
-
   ground.velocityX = 0;
   player.velocityY = 0;
   //bg.velocityX=0;
-
+  GameOver.visible = true;
   obstacleGroup.setVelocityXEach(0);
   birdGrp.setVelocityXEach(0);
   
@@ -138,14 +162,9 @@ if(obstacleGroup.isTouching(player)||birdGrp.isTouching(player)){
   if(mousePressedOver(GameOver)) {
     reset();
   }
-  GameOver.visible = true;
- }
-
-  drawSprites();
-  textSize(30)
-  fill("red")
-  text("Score " + score, 1200,80)
-  
+ 
+}
+drawSprites();
 }
 function enemy(){
   
@@ -184,19 +203,26 @@ function reset(){
   obstacleGroup.destroyEach();
   birdGrp.destroyEach();
   player.changeAnimation("run", runImg);
+  
+  // if(localStorage["HighestScore"]<score){
+  //   localStorage["HighestScore"] = score;
+  // }else{
+  //   text("Highest Score = "+ localStorage["HighestScore"], 400,200);
+  // }
+  
+ console.log(localStorage["HighestScore"]);
   score = 0;
-
-
 }
-function sliding(){
-  slide = setInterval(slideFunc, 3000);
+function points(){
+  if(frameCount%230===0){
+  coin=createSprite(1370,random(110,450),20,20);
+  coin.velocityX=-5;
+  coin.scale=0.15;
+  coin.addImage("coin",coinImg);
+  coin.lifetime=400;
+  coinGroup.add(coin);
+  GameOver.depth=coin.depth;
+  GameOver.depth=GameOver.depth+1;
   }
-function slideFunc(){
-  alert("hello")
-
-    //player.addAnimation("slide",slideImg);
-
 }
-//  call sliding funciton on specific conditon
-//didnt get you sir
-// when you want to use this funciton
+
